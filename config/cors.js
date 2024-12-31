76ac63584aaa12b1c7537e7175acba518f1b85d0
Максим <acoustic.sdk@gmail.com>
2024-12-31 16:39:05 +0200

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export const corsConfig = cors({
-    origin: '*', // Разрешаем все источники
+    origin: '*', // Разрешаем ВРЕМЕННО все источники
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
