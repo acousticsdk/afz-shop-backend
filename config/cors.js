da5ec936fde2baf307f37740e98e4ba47c2ac642
Максим <acoustic.sdk@gmail.com>
2024-12-31 16:38:41 +0200

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,21 @@
-export const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+import cors from 'cors';
+
+export const corsConfig = cors({
+    origin: '*', // Разрешаем все источники
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
-    optionsSuccessStatus: 200
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+});
+
+// Middleware для предварительных запросов OPTIONS
+export const handlePreflight = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+        res.header('Access-Control-Max-Age', '86400'); // 24 часа
+        return res.status(204).send();
+    }
+    next();
 };
